b08b8ddee75823d35a2360d6fd029e1ba9621d4d
Alvin <>
2020-12-07 10:01:39 +0400

    building and using a dropdown directive

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, HostListener, HostBinding } from "@angular/core";
+
+@Directive({
+  selector: '[appDropdown]',
+  exportAs: 'appDropDown'
+})
+export class DropdownDirective {
+  private wasInside = false;
+
+  @HostBinding('class.show') isOpen = false;
+
+  // tslint:disable-next-line:typedef
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+    this.wasInside = true;
+  }
+
+  // tslint:disable-next-line:typedef
+  @HostListener('document:click') clickout() {
+    if (!this.wasInside) {
+      this.isOpen = false;
+    }
+    this.wasInside = false;
+  }
+}
